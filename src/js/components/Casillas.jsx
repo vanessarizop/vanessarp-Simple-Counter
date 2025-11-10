90fd9a59a55b61805b9789e6a690b665df3889ea
vanessarizop <vanessa.rizop@gmail.com>
2025-11-10 11:22:03 -0500

    Fix class name for Casillas component

--- src/js/components/Casillas.jsx
+++ src/js/components/Casillas.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Casillas = (props) => {
   return (
     <>
-    <div className="ccard d-inline-flex flex-row m-3 bg-dark text-light text-letterC "> 
+    <div className="card d-inline-flex flex-row m-3 bg-dark text-light text-letterC "> 
       <div className="card  p-3 bg-secondary text-light m-3 font-weight-bold" >{props.watchIcon}</div>
       <div className="card  p-3 bg-secondary text-light  m-3">{props.Six}</div>
       <div className="card  p-3 bg-secondary text-light  m-3">{props.Five}</div>
